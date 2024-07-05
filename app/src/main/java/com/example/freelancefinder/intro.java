6b0cf8a75cdbff999a0f839575ff2a3269aa244e
Drockz-Tech <167034703+Drockz-Tech@users.noreply.github.com>
2024-07-05 10:23:20 +0530

    First commit -1

---
+++ app/src/main/java/com/example/freelancefinder/intro.java
@@ -0,0 +1,100 @@
+package com.example.freelancefinder;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class intro extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private Button btnlogin;
+    private Button btnRegistration;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
+    Timer time;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro2);
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
+        time = new Timer();
+        time.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(intro.this, login.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 2000);
+
+
+
+    }
+
+    private void LoginFunction() {
+
+        email = findViewById(R.id.loginkamail);
+        password = findViewById(R.id.loginkapassword);
+        btnlogin = findViewById(R.id.loginkaloginbutton);
+        btnRegistration = findViewById(R.id.register_btn_l);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mEmail = email.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                if (TextUtils.isEmpty(mEmail)){
+                    email.setError("Required Field...");
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)){
+                    password.setError("Required Field...");
+                }
+                mDialog.setMessage("Processing....");
+                mDialog.show();
+                mAuth.signInWithEmailAndPassword(mEmail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Home_Activity.class));
+                            mDialog.dismiss();
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),"Login Failed....", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), registration_activity.class));
+
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/freelancefinder/login.java
@@ -0,0 +1,114 @@
+package com.example.freelancefinder;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class login extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private Button btnlogin;
+    private Button btnRegistration;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
+    private Button btngoogle;
+    private Button btnfacebook;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
+
+
+        LoginFunction();
+
+
+    }
+    private void LoginFunction() {
+
+        email = findViewById(R.id.loginkamail);
+        password = findViewById(R.id.loginkapassword);
+        btnlogin = findViewById(R.id.loginkaloginbutton);
+        btnRegistration = findViewById(R.id.register_btn_l);
+        btngoogle = findViewById(R.id.button3);
+        btnfacebook = findViewById(R.id.button2);
+        btngoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://accounts.google.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+        btnfacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.facebook.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mEmail = email.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                if (TextUtils.isEmpty(mEmail)){
+                    email.setError("Required Field...");
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)){
+                    password.setError("Required Field...");
+                }
+                mDialog.setMessage("Processing....");
+                mDialog.show();
+                mAuth.signInWithEmailAndPassword(mEmail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Home_Activity.class));
+                            mDialog.dismiss();
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),"Login Failed....", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), registration_activity.class));
+
+            }
+        });
+
+
+    }
+
+}
+
