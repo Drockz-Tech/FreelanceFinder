19944ec25720c72d016dd24df4bc0f8a2fc6255c
krithika1906 <krithika.umasankar@gmail.com>
2024-07-10 18:52:42 +0530

    FreeLance DataBase

---
+++ app/src/androidTest/java/com/example/freelancefinder/FreelanceDao.kt
@@ -0,0 +1,20 @@
+package com.example.freelancefinder
+
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.freelancefinder.JobToSave
+
+
+@Dao
+interface RemoteJobDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertJob(job: JobToSave): Long
+
+    @Query("SELECT * FROM job ORDER BY id DESC")
+    fun getAllJob(): LiveData<List<JobToSave>>
+
+    @Delete
+    suspend fun deleteJob(job: JobToSave)
+}
